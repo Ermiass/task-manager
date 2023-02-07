11cb1ac07168b2cabaa464687901681be92b270a
Ermiass <Erohailu@gmail.com>
2023-02-07 17:10:51 -0500

    feat(add-task):

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,31 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+text!: string;
+day!:string;
+reminder!:boolean;
+onSubmit() {
+  if(!this.text){
+    alert('please add a task!')
+    
+  }
+  const newTask = {
+    text: this.text,
+    day:this.day,
+    reminder: this.reminder
+  }
+  this.onAddTask.emit(newTask)
+  this.text = '';
+  this.day  = '';
+  this.reminder = false;
+
+}
+}
