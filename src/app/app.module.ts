bb59ab381c7d04f01901fb8af59348bf2e214f2c
Ermiass <Erohailu@gmail.com>
2023-02-07 20:08:58 -0500

    feat(routes):added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from '@angular/common/http';
-import { AddTaskComponent } from './components/add-task/add-task.component'
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component'
+
+const appRoutes: Routes = [
+  { path: '', component:TasksComponent
+},
+{ path: 'about', component:AboutComponent
+}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,13 +27,17 @@ import { AddTaskComponent } from './components/add-task/add-task.component'
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
+
 
   ],
   providers: [],
